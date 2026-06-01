b064aa3c04156375408ba3116a9bbc56c3d9785a
Francisco Manuel Medina García <medinafran@gmail.com>
2026-06-01 14:54:59 +0100

    Creacción de un backlog general en Clase principal para gestionar funcionalidades a implementar

--- src/main/java/org/example/cursospring/rapidito/RapiditoApplication.java
+++ src/main/java/org/example/cursospring/rapidito/RapiditoApplication.java
@@ -10,4 +10,12 @@ public class RapiditoApplication {
         SpringApplication.run(RapiditoApplication.class, args);
     }
 
+
+    // Backlog general:
+    // Todo: - Añadir documentación de la API con Swagger/OpenAPI
+    // Todo:  - Implementar seguridad con Spring Security (JWT)
+    // - Añadir logging con SLF4J y Logback - Revisar si ya no hay problemas de seguridad o buscar alternativa actual
+    // Todo:  - Implementar pruebas unitarias con JUnit y Mockito
+    // Todo:  - Implementar pruebas de integración con Spring Boot Test
+
 }
